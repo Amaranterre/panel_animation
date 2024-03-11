164253d99ea83aca2cfc03858fb1258c4799bfe7
Miver <amarantagrf@gmail.com>
2024-03-11 15:07:52 +0800

    new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.vue 	new file:   src/assets/panel_img/animated/extend/chunk1.png 	new file:   src/assets/panel_img/animated/extend/chunk2.png 	new file:   src/assets/panel_img/animated/extend/chunk3.png 	new file:   src/assets/panel_img/animated/extend/chunk4.png 	new file:   src/assets/panel_img/animated/extend/line1.png 	new file:   src/assets/panel_img/animated/extend/line2.png 	new file:   src/assets/panel_img/animated/extend/line3.png 	new file:   src/assets/panel_img/animated/extend/line4.png 	new file:   src/assets/panel_img/static/chunk0.png 	new file:   src/assets/panel_img/static/comp0.png 	new file:   src/assets/vue.svg 	new file:   src/components/HelloWorld.vue 	new file:   src/components/panel/selector.vue 	new file:   src/main.js 	new file:   src/style.css 	new file:   vite.config.js

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import Panel from './components/panel/selector.vue'
+</script>
+
+<template>
+  <div class="bg-frame">
+    <div class="bg-core">
+      <Panel />
+    </div>
+  </div>
+  
+</template>
+
+
+<style>
+.bg-frame {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -10;
+  zoom: 1;
+  background-color: rgb(0, 0, 0);
+}
+
+.bg-core {
+  position: fixed;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -10;
+  zoom: 1;
+  background-size: cover;
+  
+}
+</style>
---
+++ src/components/panel/selector.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { ref } from 'vue';
+// import chunk0 from '/src/assets/panel_img/static/chunk0.png';
+// import comp0 from '/src/assets/panel_img/static/comp0.png';
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="out">
+    <img class="static_picture1" src="/src/assets/panel_img/static/chunk0.png">
+    <img class="static_picture2" src="/src/assets/panel_img/static/comp0.png">
+  </div>
+  
+</template>
+
+<style>
+  .out {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start; /* 靠左对齐 */
+    height: 100%; /* 确保容器有足够的高度 */
+    
+    
+    
+  }
+  .picture {
+    /* position:absolute; */
+
+    height: 70%;
+    width: auto;
+  }
+  .static_picture1 {
+    position:absolute;
+    display: flex;
+
+    height: 70%;
+    width: auto;
+
+    z-index: 0;
+  }      
+  .static_picture2 {
+    position:absolute;
+    display: flex;
+
+    height: 70%;
+    width: auto;
+
+    z-index: 1;
+  }
+</style>
+
